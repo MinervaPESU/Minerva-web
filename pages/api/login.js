ce80f0d326f53e141f3e30617396a89640fd858a
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2025-08-21 09:51:45 +0530

    make JWT signing key private

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,12 +5,16 @@ import jwt from "jsonwebtoken";
 const validate = async (req, res) => {
   // Validation for the login page
   if (req.method == "POST") {
-    console.log(process.env.NEXT_PUBLIC_API);
+    console.log(process.env.SERVER_SECRET_SIGN);
     const user = await User.findOne({ email: req.body.email });
     if (user.password == req.body.password) {
-      const token = jwt.sign({ _id: user._id }, process.env.NEXT_PUBLIC_API, {
-        expiresIn: "1d",
-      });
+      const token = jwt.sign(
+        { _id: user._id },
+        process.env.SERVER_SECRET_SIGN,
+        {
+          expiresIn: "1d",
+        }
+      );
       res.json({ token: token });
     } else {
       res.json({ token: "" });
